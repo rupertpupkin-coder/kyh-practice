0fe41f7c2a024c4d13236471bd1d0f9f5c1a80dd
Jacob <jacob.nordlund@student.kyh.se>
2020-09-01 10:40:35 +0200

    Gissa nummer + antal gissningar

--- guess_number.py
+++ guess_number.py
@@ -1,18 +1,21 @@
 import random
 
-n = random.randint(1, 20)
-print("I'm thinking of a number between 1 and 20. Guess which?")
+n = random.randint(1, 100)
+print("Jag tänker på ett nummer mellan 1-100. Gissa vilket?")
+guesses = 1
 
 while True:
-    text = input("Your guess: ")
+    text = input("Din gissning: ")
     as_number = int(text)
 
     if as_number == n:
-        print("Correct!")
+        print("Snyggt! Det tog dig " + str(guesses) + " gissningar!")
         break
 
     if as_number < n:
-        print("Wrong, my number is higher... Try again!")
+        print("Fel, mitt nummer är högre... Försök igen!")
+        guesses = guesses + 1
 
     if as_number > n:
-        print("Wrong, my number is lower... Try again!")
+        print("Fel, mitt nummer är lägre... Försök igen!")
+        guesses = guesses + 1
