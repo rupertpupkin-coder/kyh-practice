fb53ac42e1043f82032c87c246eae2df9dbeae97
Jacob <jacob.nordlund@student.kyh.se>
2020-09-01 13:02:57 +0200

    trying with ask_number

--- guess_number.py
+++ guess_number.py
@@ -3,23 +3,23 @@ import random
 n = random.randint(1, 100)
 print("Jag tänker på ett nummer mellan 1-100. Gissa vilket?")
 guesses = 1
+
+
 def ask_number():
     text = input("Vad är din gissning?")
     tal = int(text)
     return tal
+
+
 ask_number()
 
 
 def mainloop(guesses):
-
-
-
     n = random.randint(1, 100)
     while True:
 
         text = input("Vad är din gissning?")
         tal = int(text)
-
 
         if tal == n:
             print("Snyggt! Det tog dig " + str(guesses) + " gissningar!")
@@ -32,5 +32,6 @@ def mainloop(guesses):
         if tal > n:
             print("Fel, mitt nummer är lägre... Försök igen!")
             guesses = guesses + 1
-mainloop(guesses)
 
+
+mainloop(guesses)
