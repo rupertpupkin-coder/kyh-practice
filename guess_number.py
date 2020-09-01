e6c2140d5098897563ceb3eec998172abc544461
Jacob <jacob.nordlund@student.kyh.se>
2020-09-01 17:19:05 +0200

    testar mainloop return

--- guess_number.py
+++ guess_number.py
@@ -1,33 +1,33 @@
 import random
 
+guesses = 0
 n = random.randint(1, 100)
 print("Jag tänker på ett nummer mellan 1-100. Gissa vilket?")
-guesses = 1
 
 
 def ask_number():
     text = input("Vad är din gissning?")
-    tal = int(text)
-    return tal
+    as_number = int(text)
+    return as_number
 
 
-def mainloop(guesses):
-    n = random.randint(1, 100)
+def mainloop(n,guesses):
     while True:
 
         as_number = ask_number()
-
+        guesses = guesses + 1
         if as_number == n:
-            print("Snyggt! Det tog dig " + str(guesses) + " gissningar!")
+            print("Snyggt!")
             break
 
         if as_number < n:
             print("Fel, mitt nummer är högre... Försök igen!")
-            guesses = guesses + 1
 
         if as_number > n:
             print("Fel, mitt nummer är lägre... Försök igen!")
-            guesses = guesses + 1
+
+            return guesses
 
 
-mainloop(guesses)
+guesses = mainloop(n,guesses)
+print("Du gissade", guesses, "antal gånger")
