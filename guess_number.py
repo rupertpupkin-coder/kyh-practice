b7bf591e072b07aab5d2903b9cc2b3f871e3e497
Jacob <jacob.nordlund@student.kyh.se>
2020-09-01 11:24:16 +0200

    fixed mainloop function

--- guess_number.py
+++ guess_number.py
@@ -4,18 +4,23 @@ n = random.randint(1, 100)
 print("Jag tänker på ett nummer mellan 1-100. Gissa vilket?")
 guesses = 1
 
-while True:
-    text = input("Din gissning: ")
-    as_number = int(text)
 
-    if as_number == n:
-        print("Snyggt! Det tog dig " + str(guesses) + " gissningar!")
-        break
+def mainloop(guesses):
+    n = random.randint(1, 100)
+    while True:
+        text = input("Din gissning: ")
+        as_number = int(text)
 
-    if as_number < n:
-        print("Fel, mitt nummer är högre... Försök igen!")
-        guesses = guesses + 1
 
-    if as_number > n:
-        print("Fel, mitt nummer är lägre... Försök igen!")
-        guesses = guesses + 1
+        if as_number == n:
+            print("Snyggt! Det tog dig " + str(guesses) + " gissningar!")
+            break
+
+        if as_number < n:
+            print("Fel, mitt nummer är högre... Försök igen!")
+            guesses = guesses + 1
+
+        if as_number > n:
+            print("Fel, mitt nummer är lägre... Försök igen!")
+            guesses = guesses + 1
+mainloop(guesses)
