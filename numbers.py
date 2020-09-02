a2c1f6498e63059ac8dacf190c1136c81d776034
Jacob <jacob.nordlund@student.kyh.se>
2020-09-02 11:30:22 +0200

    använder f-stringen för resultatet.

--- numbers.py
+++ numbers.py
@@ -1,30 +1,24 @@
-import calc
-from calc import multiply, divide
+from calc import add, subtract, multiply, divide
 
 
 def run():
     print("This program will help with simple calculations. What do you want to do?")
+    print("1 - add numbers")
+    print("2 - subtract numbers")
+    print("3 - multiply numbers")
+    print("4 - divide numbers")
+    answer = input(">> ")
+    a = int(input("A="))
+    b = int(input("B="))
+    if answer == "1":
+        result = add(a, b)
+    if answer == "2":
+        result = subtract(a, b)
+    if answer == "3":
+        result = multiply(a, b)
+    if answer == "4":
+        result = divide(a, b)
+    print(f"{result}")
 
-
-print("1 - add numbers")
-print("2 - subtract numbers")
-print("3 - multiply numbers")
-print("4 - divide numbers")
-answer = input(">> ")
-a = int(input("A="))
-b = int(input("B="))
-if answer == "1":
-    result = calc.add(a, b)
-
-if answer == "2":
-    result = calc.subtract(a, b)
-
-if answer == "3":
-    result = calc.multiply(a, b)
-
-if answer == "4":
-    result = calc.divide(a, b)
-
-print("Result = " + str(result))
 if __name__ == '__main__':
     run()
