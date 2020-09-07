281a1a7eb80d6f928bc6e7454568a1d7d115bb39
Jacob <jacob.nordlund@student.kyh.se>
2020-09-07 10:35:29 +0200

    split funktion

--- uppgift12.py
+++ uppgift12.py
@@ -5,6 +5,7 @@ def is_it_too_long(name):
 
 def main():
     students = ["anna", "beatrice", "cecilia", "doris", "esmeralda", "frida", "gunilla"]
+    students = input("Ange studenternas namn med komma emellan: ").split()
     for name in students:
         if is_it_too_long(name):
             print(f"{name} är för långt!")
