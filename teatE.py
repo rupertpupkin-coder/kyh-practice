da1cecf9eb3431649f1565827d3446ce68948a1b
Jacob <jacob.nordlund@student.kyh.se>
2020-09-07 09:34:35 +0200

    räkna

---
+++ teatE.py
@@ -0,0 +1,11 @@
+def main():
+
+    num1 = input("First number: ")
+    num2 = input("Second: ")
+    result = float(num1) + float(num2)
+
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
