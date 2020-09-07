b826e14398fcea3a3d40de7031a0bb4e1715db48
Jacob <jacob.nordlund@student.kyh.se>
2020-09-07 11:25:49 +0200

    try except

--- uppgift12.py
+++ uppgift12.py
@@ -1,15 +1,22 @@
-def is_it_too_long(name):
+def is_it_too_long(name, max_length):
 
-    return len(name) > 5
+    return len(name) > max_length
 
 
-def main():
-    students = ["anna", "beatrice", "cecilia", "doris", "esmeralda", "frida", "gunilla"]
+def main(name):
+    # students = ["anna", "beatrice", "cecilia", "doris", "esmeralda", "frida", "gunilla"]
     students = input("Ange studenternas namn med komma emellan: ").split(",")
     for name in students:
-        if is_it_too_long(name):
+        if is_it_too_long(name, max_length):
             print(f"{name.title()} är för långt!")
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        max_length = int(input("Hur långt får namnet vara?"))
+    except ValueError:
+        name = 5
+
+
+
+    main(max_length)
