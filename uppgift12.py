134a331202f724b6d82c40ebbbd4f0a86cb8ee5a
Jacob <jacob.nordlund@student.kyh.se>
2020-09-07 10:37:59 +0200

    Stor bokstav

--- uppgift12.py
+++ uppgift12.py
@@ -5,10 +5,10 @@ def is_it_too_long(name):
 
 def main():
     students = ["anna", "beatrice", "cecilia", "doris", "esmeralda", "frida", "gunilla"]
-    students = input("Ange studenternas namn med komma emellan: ").split()
+    students = input("Ange studenternas namn med komma emellan: ").split(",")
     for name in students:
         if is_it_too_long(name):
-            print(f"{name} är för långt!")
+            print(f"{name.title()} är för långt!")
 
 
 if __name__ == '__main__':
