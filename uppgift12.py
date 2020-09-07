2bf5622b94acb9d85afcb3173c8f0e1580fb8594
Jacob <jacob.nordlund@student.kyh.se>
2020-09-07 11:49:03 +0200

    name.strip

--- uppgift12.py
+++ uppgift12.py
@@ -3,20 +3,21 @@ def is_it_too_long(name, max_length):
     return len(name) > max_length
 
 
-def main(name):
+def main():
     # students = ["anna", "beatrice", "cecilia", "doris", "esmeralda", "frida", "gunilla"]
+    try:
+        max_length = int(input("Hur långt får namnet vara?"))
+    except ValueError:
+        max_length = 5
+
     students = input("Ange studenternas namn med komma emellan: ").split(",")
+
+
     for name in students:
+        name = name.strip()
         if is_it_too_long(name, max_length):
             print(f"{name.title()} är för långt!")
 
 
 if __name__ == '__main__':
-    try:
-        max_length = int(input("Hur långt får namnet vara?"))
-    except ValueError:
-        name = 5
-
-
-
-    main(max_length)
+    main()
