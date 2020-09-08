162af993e3e4a8bfa2e031c3e8e0c850345b99d2
Jacob <jacob.nordlund@student.kyh.se>
2020-09-08 10:38:25 +0200

    adderat kategori

--- uppgift14.py
+++ uppgift14.py
@@ -1,14 +1,17 @@
 FRUITS = ['banana', 'apple', 'orange']
 CARS = ['volvo', 'ford', 'tesla']
-
+COLOR = ["red", "blue", "green", "yellow", "black"]
 
 
 def run():
-
-    basket = ['volvo', 'is', 'an', 'orange', 'apple']
+    basket = ['volvo', 'is', 'red', 'an', 'orange', 'apple', 'black']
     cars = []
+
     fruits = []
+
+    color = []
     rest = []
+
     for item in basket:
         if item in CARS:
             cars.append(item)
@@ -16,21 +19,24 @@ def run():
         elif item in FRUITS:
             fruits.append(item)
 
+        elif item in COLOR:
+            color.append(item)
+
         else:
             rest.append(item)
 
-    write_things(sorted(cars), 'Cars')
+    write_things(sorted(cars), 'Cars')  # Skickar in en sorterad lista sorted(cars) + namnet.
     write_things(sorted(fruits), 'Fruits')
+    write_things(sorted(color), "Colors")
     write_things(sorted(rest), 'Misc')
 
 
-
-
 def write_things(items, kind):
-    print(f"{kind.upper()} ({len(items)}st)")
+    print(f"{kind.upper()}  ({len(items)}st)")
     for item in items:
-        print(f"{item}")
+        print(f" {item}")
 
 
 if __name__ == '__main__':
+
     run()
