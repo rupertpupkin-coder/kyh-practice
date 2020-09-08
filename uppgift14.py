d2bcde4332e94c97df792d0ac3ddf4220d5ad583
Jacob <jacob.nordlund@student.kyh.se>
2020-09-08 10:06:15 +0200

    len items för att räkna hur många items i varje kategori

---
+++ uppgift14.py
@@ -0,0 +1,29 @@
+FRUITS = ['banana', 'apple', 'orange']
+CARS = ['volvo', 'ford', 'tesla']
+
+
+def run():
+    basket = ['volvo', 'is', 'an', 'orange', 'apple']
+    cars = []
+    fruits = []
+    rest = []
+    for item in basket:
+        if item in CARS:
+            cars.append(item)
+        elif item in FRUITS:
+            fruits.append(item)
+        else:
+            rest.append(item)
+    write_things(cars, 'Cars')
+    write_things(fruits, 'Fruits')
+    write_things(rest, 'Misc')
+
+
+def write_things(items, kind):
+    print(f"{kind.upper()} ({len(items)}st)")
+    for item in items:
+        print(f"{item}")
+
+
+if __name__ == '__main__':
+    run()
