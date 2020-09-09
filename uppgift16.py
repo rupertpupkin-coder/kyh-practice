72f0dfb6257f1fb57163deb2563c3a06fcad5e8f
Jacob <jacob.nordlund@student.kyh.se>
2020-09-09 09:40:06 +0200

    3 lösningar

--- uppgift16.py
+++ uppgift16.py
@@ -21,7 +21,7 @@ def case():
 
 if __name__ == '__main__':
     case()
-"""
+
 from pathlib import Path
 
 
@@ -43,3 +43,16 @@ def case():
 
 
 case()
+
+"""
+from pathlib import Path
+
+
+def run():
+    for line in Path("system.log").read_text().splitlines():
+        if "BEAR" in line or "X-RAY" in line:
+            print(line)
+
+
+if __name__ == '__main__':
+    run()
