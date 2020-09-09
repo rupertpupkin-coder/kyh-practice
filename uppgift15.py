571f2d4e23a69e8c74a6943d9e8498a6d0329e65
Jacob <jacob.nordlund@student.kyh.se>
2020-09-09 10:28:29 +0200

    godtycklig text

--- uppgift15.py
+++ uppgift15.py
@@ -1,7 +1,19 @@
 def wordcount():
 
-    text = input("Skriv en text: ").split()
-    print(f"Du skrev {len(text)} ord")
+    # text = input("Skriv en text: ").split()
+    # print(f"Du skrev {len(text)} ord")
+    intervju = """– Det är en slags inflammatorisk skada i ryggmärgen. Det kan orsakas av infektioner, inflammationer eller liknande, säger Matti Sällberg, biomedicinsk analytiker och professor vid Karolinska universitetet.
+Om försökpersonens sjukdom är en biverkning av vaccinet är ännu för tidigt att slå fast.
+– Man ska ha klart för sig att när man vaccinerar så pass många som man gör i dessa studierna, kanske 10 000, 30 000 eller 50 000 personer, så är det inte konstigt om sådant här inträffar helt oberoende av vaccinationen.
+"""
+    vokaler = "a,e,i,o,u,y,å,ä,ö"
+    count = 0
+
+    for char in intervju:
+        if char in vokaler:
+            count += 1
+
+    print(count)
 
 
 if __name__ == '__main__':
