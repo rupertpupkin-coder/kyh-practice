c738a228e3c20ea73ed9cbdfda43d3196a7f336b
Jacob <jacob.nordlund@student.kyh.se>
2020-09-10 09:45:09 +0200

    todo list trying and trying

--- uppgift17.py
+++ uppgift17.py
@@ -1,3 +1,50 @@
+# from pathlib import Path
+#
+# p = Path("TODO.txt")
+# context = p.read_text(encoding='utf8').splitlines()
+#
+#
+#
+# homepage = """
+# 1. Lista TODO
+# 2. Lägg till uppgift
+# 3. Ta bort uppgift
+# 4. Avbryt programmet"""
+#
+# print(homepage)
+#
+# todo_list = []
+# todo_list.append(context)
+#
+#
+# def user_input():
+#     user_answer = input("Vad vill du göra? ")
+#     while True:
+#         if user_answer == "1":
+#             print(context)
+#
+#         if user_answer == "2":
+#             p.read_text()
+#             user_input2 = input("Vad vill du lägga till? ")
+#             todo_list.append(user_input2)
+#              p.write_text(f"{todo_list}")
+#
+#         if user_answer == "3":
+#             print(p.read_text())
+#             user_input3 = input("Vad vill du ta bort? ")
+#             todo_list.remove(user_input3)
+#             p.write_text(f"{todo_list}")
+#
+#         if user_answer == "4":
+#
+#             quit()
+#
+#
+#         user_input()
+#
+# user_input()
+
+
 from pathlib import Path
 
 p = Path("TODO.txt")
@@ -21,14 +68,11 @@ def user_input():
         if user_answer == "1":
             print(p.read_text())
 
-
         if user_answer == "2":
-
             p.read_text()
             user_input2 = input("Vad vill du lägga till? ")
             todo_list.append(user_input2)
             p.write_text(f"{todo_list}")
-
 
         if user_answer == "3":
             print(p.read_text())
