b7e684aac854a69ebd71ec08b4ac0d909d654002
Jacob <jacob.nordlund@student.kyh.se>
2020-09-10 11:09:55 +0200

    main funktion

--- uppgift18.py
+++ uppgift18.py
@@ -1,16 +1,18 @@
+def main():
+    people = {
+        "Fredrik": "0702778511",
+        "Olof": "123456789",
+        "Lisa": "9999999999",
+        "Bodil": "555-666-789",
+    }
+    vem = input(f"Jag har {len(people)} kontakter i telefonkatalogen. Vem vill du ringa?")
+    if vem not in people:
+        print("Sorry hörru, vet ej vem detta är. Har endast VIP i min katalog")
+    else:
+        number = people[vem]
+        print(f"Numret till {vem} är {number}")
 
-people = {
-    "Fredrik": "0702778511",
-    "Olof": "123456789",
-    "Lisa": "9999999999",
-    "Bodil": "555-666-789",
-}
 
+__name__ = "__main__"
 
-vem = input(f"Du har {len(people)} kontakter. Vem vill du ringa?")
-
-if vem not in people:
-    print("Sorry hörru, vet ej vem detta är. Har endast VIP i min katalog")
-else:
-    number = people[vem]
-    print(f"Numret till {vem} är {number}")
+main()
