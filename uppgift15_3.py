2ea3c524bcde003578623dbc5af61e49dd32cb2b
Jacob <jacob.nordlund@student.kyh.se>
2020-09-11 20:59:10 +0200

    lagt till löpare

---
+++ uppgift15_3.py
@@ -0,0 +1,12 @@
+def main():
+    runners_in_order = "Love, Lasse, Louise, Leopold, Lisa, Lova, Lennart, Lena, Lisette, Linus".split()
+    vem = input("Ange löpare du söker placering för: ")
+
+    for index, order in runners_in_order:
+        runners_in_order
+
+
+
+
+if __name__ == '__main__':
+        main()
