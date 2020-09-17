2a1287d914bebead4ccb5eb40027b1e27360c311
Jacob <jacob.nordlund@student.kyh.se>
2020-09-17 10:32:34 +0200

    rightalign

---
+++ uppgift27.py
@@ -0,0 +1,21 @@
+import json
+from pathlib import Path
+
+p = Path("data.json")
+content = p.read_text(encoding=("utf8"))
+data = json.loads(content)
+
+
+def main():
+    print("Innehåll:                          100g")
+    for elem in data:
+        if elem['rightalign']:
+
+            print(f"{elem['what']:>20} {elem['value']:17}{elem['unit']}")
+
+        elif not elem['rightalign']:
+            print(f"{elem['what']:20} {elem['value']:17}{elem['unit']}")
+
+
+if __name__ == '__main__':
+    main()
