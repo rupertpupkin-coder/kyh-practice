5e778213537aea788c4fa096835f4b4af977fb5b
Jacob <jacob.nordlund@student.kyh.se>
2020-09-24 11:57:35 +0200

    uppgift36 uppdaterad

--- uppgift36/main.py
+++ uppgift36/main.py
@@ -2,5 +2,19 @@ from uppgift36.pwstrength import compute_strength
 
 if __name__ == '__main__':
 
-    password = input("Ange lösenord: ")
-    compute_strength(password)
+    while True:
+        password = input("Ange lösenord: ")
+        res, passed = compute_strength(password)
+        if passed:
+            if res == 0:
+                print(f"Antal poäng: {res}. Svagt lösenord men godkänt.")
+            if res == 1:
+                print(f"Antal poäng: {res} Bättre kan du.")
+            if res > 1:
+                print(f" Antal poäng: {res}. Bra lösenord")
+            break
+        print("Ogiltigt lösenord! Försök igen :)")
+
+
+
+
--- uppgift36/pwstrength.py
+++ uppgift36/pwstrength.py
@@ -8,11 +8,12 @@
 
 def compute_strength(pw):
     num_points = 0
+    passed = True
 
     # a) om lösenordet är mer än 10 tecken långt får det +1 poäng
     if len(pw) > 10:
         num_points += 1
-        print("1 poäng - Längre än 10 tecken")
+        # print("1 poäng - Längre än 10 tecken")
 
     # b) om lösenordet innehåller både siffror och bokstäver (det räcker med de engelska bokstäverna)
     # så får det +1 poäng
@@ -20,14 +21,14 @@ def compute_strength(pw):
 
         if any(char.isalpha() for char in pw):
             num_points += 1
-            print("1 poäng - Innehåller både siffror och bokstäver")
+            # print("1 poäng - Innehåller både siffror och bokstäver")
 
     # c) om lösenordet innehåller någon av symbolerna “#%&+_-” får det +1 poäng
 
     special_characters = "#%&+_-"
     if any(char in special_characters for char in pw):
         num_points += 1
-        print("1 poäng - Innehåller specialtecken")
+        # print("1 poäng - Innehåller specialtecken")
 
     # d) om lösenordet innehåller något annat tecken än bokstäver, siffror och symbolerna i (3)
     # är det ogiltigt och får 0 poäng.
@@ -35,7 +36,8 @@ def compute_strength(pw):
     if not (any(char.isdigit() for char in pw)
             or any(char.isalpha() for char in pw)
             or any(char in special_characters for char in pw)):
-        print("Ogiltligt lösenord!")
+        #print("Ogiltligt lösenord!")
         num_points = 0
-
-    print(f"\nAntal poäng: {num_points}")
+        passed = False
+    return num_points, passed
+    #print(f"\nAntal poäng: {num_points}")
