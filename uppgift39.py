45602d67d3e5d4f1ebd86a17886a813cc8c1c5f4
Jacob <jacob.nordlund@student.kyh.se>
2020-09-28 14:51:38 +0200

    färdig

---
+++ uppgift39.py
@@ -0,0 +1,39 @@
+# 39.1
+
+def maximum(a, b, c):
+    result = a
+    if result < b:
+        result = b
+    if result < c:
+        result = c
+    return result
+
+
+ls = maximum(1, 100, 40)
+print(ls)
+
+
+# 39.2
+
+def add(addList):
+    result = 0
+    for i in addList:
+        result += i
+    return result
+
+
+d = add([100, 2212, 30])
+print(d)
+
+
+# 39.3
+
+def multiply(a):
+    result = 1
+    for i in a:
+        result *= i
+    return result
+
+
+f = multiply([1, 2, 3])
+print(f)
---
+++ uppgift40.py
@@ -0,0 +1,46 @@
+# 40.1
+def reverse(a):
+    text = []
+    for c in a:
+        text.append(c)
+    length = len(text)
+    s = length
+    new_list = [None] * length
+    for i in text:
+        s = s - 1
+        new_list[s] = i
+    return new_list
+
+
+o = "".join(reverse("123456"))
+print(o)
+
+
+# 40.2
+
+def text(text):
+    count = 0
+    for l in text:
+        if l == l.upper():
+            count += 1
+    return count
+
+
+name = input("Skriv något:")
+a = text(name)
+print(a)
+
+
+# 40.3
+
+def minmax(value, min, max):
+    if value < min:
+        return False
+    if value > max:
+        return False
+    else:
+        return True
+
+
+a = minmax(60, 10, 56)
+print(a)
